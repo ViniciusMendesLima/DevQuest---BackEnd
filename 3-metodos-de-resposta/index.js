11dc1680dbff33682e00e3e6a3e25364e11b6ed3
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-09-16 18:10:22 -0300

    metodos de resposta

---
+++ 3-metodos-de-resposta/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+const users = [
+    {id: 1, name: 'John Doe'},
+    {id: 2, name: 'Jane Doe'},
+    {id: 3, name: 'Bob Smith'},
+];
+
+app.get('/users/:id', (req, res) => {
+    return res.redirect('https://google.com')
+    
+    // const id = req.params.id;
+    // const user = users.find (u => u.id == id);
+
+    // if(!user) {
+    //     return res.status(404).send('User not found');
+    // }
+
+    // return res.json(user);
+})
+
+app.listen(3000, () => {
+  console.log("Servidor em execução em http://localhost:3000/");
+});
