dee176e4587f2356785732e58f06399b3bdba3e7
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-09-16 17:39:01 -0300

    rotas

--- 1-modulo-node/5_criando-servidor/server.js
+++ 1-modulo-node/5_criando-servidor/server.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res)=> {
 
     if(req.url === '/'){
         res.statusCode = 200;
-        res.end('Home page batata')
+        res.end('Home page!!!!')
     } else if(req.url === '/about'){
         res.statusCode = 200;
         res.end('About page')
