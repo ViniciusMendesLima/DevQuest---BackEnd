4bc621f0d54ba7d50c949ceeb6ae0e96c0204519
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-09-17 15:51:39 -0300

    rotas metodos diferentes

---
+++ 4-rotas-metodos-diferentes/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+
+app.get('/usuarios/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Este é  perfil do usuário ${name}`);
+})
+
+app.put('/usuarios/:name',(req,res) => {
+    const name = req.params.name;
+    res.send(`Este é o perfil do usuário ${name} usando o PUT`)
+
+})
+
+app.listen(3000, () => {
+  console.log("Servidor em execução em http://localhost:3000/");
+});
