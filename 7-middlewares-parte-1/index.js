f5173e924b2d81d88c088353cd8921607dc82ac7
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-09-18 17:06:03 -0300

    Application-level middleware

---
+++ 7-middlewares-parte-1/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+  const isLogged = false;
+
+  if (isLogged) {
+    next();
+  } else {
+    res
+      .status(401)
+      .send("Você precisa estar logado para acesssar essa página!");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Olá mundo!");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor iniciado na porta http://localhost:3000");
+});
